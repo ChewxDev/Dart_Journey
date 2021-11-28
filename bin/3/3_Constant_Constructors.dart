2db6684a08529dba2b0b9fb50c86008e7e003986
chewx <nicholascents77@gmail.com>
2021-11-28 15:09:52 +0100

    Constant Constructors

--- bin/3/3_Constant_Constructors.dart
+++ bin/3/3_Constant_Constructors.dart
@@ -1,4 +1,4 @@
-//! CONSTANT CONSTRUCTORS
+//! CONSTANT CONSTRUCTORS - are cononical instances, which means no matter how many instances you create, as long as the properties used to create them are the same, dart will only see a single instance
 
 //The final keyword makes it unnecessary to use the _ symbol to make a variable private, it makes the variable immutable
 class SmartPhone {
@@ -14,6 +14,8 @@ class SmartPhone {
 }
 
 void main() {
+
+  //Hence, Dart would not allocate new memory for each of these instances.
   var smartPhone = SmartPhone(brand: 'Samsung', price: 200);
   var smartPhone1 = SmartPhone(brand: 'Samsung', price: 200);
   var smartPhone2 = SmartPhone(brand: 'Samsung', price: 200);
