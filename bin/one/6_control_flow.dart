3ef56432f59bbe2ea57aae641023f952d28baea9
chewx <nicholascents77@gmail.com>
2021-11-28 02:31:52 +0100

    Loops

--- bin/one/6_control_flow.dart
+++ bin/one/6_control_flow.dart
@@ -29,7 +29,7 @@ void main() {
   //* variable scope
   // Where is the variable visible and usable
   if (age > 18) {
-    const name = 'Chewx';
+    const name = 'Chewx';//This variable ca only be used inside the if statement, hence it is a local variable
     print('Hello $name, you are of legal age1');
   }
   // print("Hello, $name");
@@ -90,7 +90,7 @@ void main() {
   }
 }
 
-enum MenuItem {
+enum MenuItem { //Creating a custom type using enum
   profile,
   settings,
 }
