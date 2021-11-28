884398ae107c2b223c53322312a4c87424746205
chewx <nicholascents77@gmail.com>
2021-11-28 01:01:50 +0100

    Statements, Expressions & Operators

---
+++ bin/one/statements_expressions_operators.dart
@@ -0,0 +1,20 @@
+void main(List<String> arguments) {
+  //Make sure to write your code in the main function
+  //Statements- These are intructions eg
+  print(
+      'Hello world!'); //Tells the computer to output this, this is a statement
+
+  //Example of a complex statement is the main function because it tells the computer where to start executing codes from and where to stop
+  //It also does not eed a semicolon because it is wrapped in curlyy braces
+
+  //Expression - Valid unit of code that resolves to a value eg
+  5; //Just a number not an instruction
+  "Name"; //Just a string not an instruction
+
+  //Arithmetic Operators
+  4 + 2; //Addition
+  4 - 2; //Subtraction
+  4 * 2; // Multiplication
+  4 / 2; //Division
+  4 ~/ 2; //Division without remainder
+}
